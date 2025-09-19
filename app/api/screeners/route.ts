1cf3fb6bc05e8d77eb007f17504ba1248ca12af0
rizzo <154651009+RogueTensor@users.noreply.github.com>
2025-09-19 16:21:04 -0600

    added dirty cheat to force no-caching on screeners route

--- app/api/screeners/route.ts
+++ app/api/screeners/route.ts
@@ -2,10 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { PrismaClient } from '@prisma/client'
-
+import { cookies } from 'next/headers'
 const prisma = new PrismaClient()
 
 export async function GET(request: NextRequest) {
+  const _cookies = cookies()
   try {
     const screeners = await prisma.screener.findMany({
       include: {
